397fc979c5c441a64ede83a221adb63c4b4012ce
Wojtek Książek <wojtek99955@gmail.com>
2022-04-21 10:36:53 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { globalStyle } from "./assets/globalStyles";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div className="App">
+      <globalStyle />
+    </div>
+  );
+}
+
+export default App;
