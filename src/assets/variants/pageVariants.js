44574374f7b88f84a30eb185668d7cf03d39f006
Wojtek Książek <wojtek99955@gmail.com>
2022-04-24 21:26:26 +0200

    pageVariants update

--- src/assets/variants/pageVariants.js
+++ src/assets/variants/pageVariants.js
@@ -1,7 +1,7 @@
 export const pageVariants = {
   hidden: {
     opacity: 0,
-    x: 250,
+    x: 150,
   },
   visible: {
     opacity: 1,
@@ -9,7 +9,7 @@ export const pageVariants = {
     transition: { duration: 0.2 },
   },
   exit: {
-    x: -250,
+    x: -150,
     transition: { ease: "easeInOut" },
     opacity: 0,
   },
